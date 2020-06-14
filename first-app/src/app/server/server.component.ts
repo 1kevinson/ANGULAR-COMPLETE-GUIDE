a00a5b4fc42291559754ccaba3fcb70ce239cd3e
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-14 15:45:28 +0200

    ngIf & ngClass directives

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -5,12 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html',
 })
 export class ServerComponent {
-  serverId: number = 10;
-  serverStatus: string = 'offline';
+  serverOneId: number = Math.floor(Math.random() * 100);
+  serverTwoId: number = Math.floor(Math.random() * 100);
+  serverStatus1: string = 'offline';
+  serverStatus2: string = 'offline';
+  fontSize = '1.5rem';
 
   username: string = '';
   inputStatus: boolean = false;
 
+  constructor() {
+    this.serverStatus1 = Math.random() > 0.5 ? 'online' : 'offline';
+    this.serverStatus2 = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
+    return [this.serverStatus1, this.serverStatus2];
+  }
+
+  getColor1() {
+    return this.serverStatus1 === 'online'
+      ? 'badge badge-success'
+      : 'badge badge-danger';
+  }
+
+  getColor2() {
+    return this.serverStatus2 === 'online'
+      ? 'badge badge-success'
+      : 'badge badge-danger';
+  }
+
+  /*
   onMatchingInput(event: Event) {
     this.inputStatus = this.username !== '';
   }
@@ -18,5 +43,5 @@ export class ServerComponent {
   onResetInput() {
     this.username = '';
     this.inputStatus = false;
-  }
+  }*/
 }
