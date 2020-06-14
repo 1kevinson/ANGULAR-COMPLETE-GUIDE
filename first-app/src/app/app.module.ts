4b01fc88c9664e042b836846af6c82c9c5cee2e9
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-14 11:13:35 +0200

    Assignement component basics

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warningalert/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ first-app/src/app/warningalert/warning.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning',
+  templateUrl: './warning.component.html',
+  styles: [
+    `
+      h2 {
+        color: darkred;
+      }
+    `,
+  ],
+})
+export class WarningComponent {}
