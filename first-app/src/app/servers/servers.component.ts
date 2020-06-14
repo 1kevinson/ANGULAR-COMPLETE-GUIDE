3d399eda176a4e5a7a8870a726417b3b13fb2ff7
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-14 10:36:15 +0200

    Fully understanding the components selectors

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  //select by class
+  //  selector: '.app-servers',
+  //select by attribute
+  //  selector: '[app-servers]',
   selector: 'app-servers',
   template: '<app-server></app-server>',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
