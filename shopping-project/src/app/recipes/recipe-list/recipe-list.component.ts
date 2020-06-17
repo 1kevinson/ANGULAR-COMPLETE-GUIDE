c2e6a0f5ec5026ff26825d49fa0a5f5f29276d45
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-17 21:27:36 +0200

    Adding content to the recipe component

--- shopping-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  //the type is array of recipes
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'Simple test',
+      'https://www.foodiecrush.com/wp-content/uploads/2019/07/Pomodoro-Sauce-foodiecrush.com-015.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'Simple test',
+      'https://images.kglobalservices.com/www.ricekrispies.com/en_us/recipe/kicrecipe-1605/' +
+        'recipe-gallery_img-5035195_recip_img-7547320_holiday_kabobs_-_rice_krispies-011.jpg'
+    ),
+  ];
 
   constructor() {}
 
