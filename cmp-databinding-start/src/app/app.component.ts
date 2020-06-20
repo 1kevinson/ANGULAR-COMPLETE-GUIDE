a083973c338b1cae8a1778409a5f5a051e0c23e6
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-20 12:20:05 +0200

    Binding to custom properties

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [
+    { type: "server", name: "Test server", content: "Just a test !" },
+  ];
 }
