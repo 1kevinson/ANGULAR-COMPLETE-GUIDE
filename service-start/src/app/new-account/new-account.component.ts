9fdba1636895932f49fe795135c934efde6983e7
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-28 19:13:30 +0200

    Using service for cross component communication

--- service-start/src/app/new-account/new-account.component.ts
+++ service-start/src/app/new-account/new-account.component.ts
@@ -12,7 +12,11 @@ export class NewAccountComponent {
   constructor(
     private loggingService: LoggingService,
     private accountService: AccountService
-  ) {}
+  ) {
+    this.accountService.statusUpdated.subscribe((status: string) =>
+      alert('New status : ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
