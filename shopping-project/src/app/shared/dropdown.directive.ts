4d38b9c065e72fa574b9ee78c5962695eab4f98d
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-28 10:59:25 +0200

    Building and using a dropdown directive

---
+++ shopping-project/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  // Bind class property
+  @HostBinding('class.open') isOpen: boolean = false;
+
+  //Add directive to listen click event on dropdown, when button clicked, call toggleOpen() method
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+}
