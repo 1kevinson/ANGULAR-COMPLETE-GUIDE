a35772948e0c1bc0d62b4aa6a711f296cf5f5e00
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-28 12:30:40 +0200

    Injecting the logging service into component

--- service-start/src/app/logging.service.ts
+++ service-start/src/app/logging.service.ts
@@ -1,5 +1,5 @@
 export class LoggingService {
   logStatusChange(status: string) {
-    console.log('A server status changed, new status:' + status);
+    console.log('A server status changed, new status: ' + status);
   }
 }
