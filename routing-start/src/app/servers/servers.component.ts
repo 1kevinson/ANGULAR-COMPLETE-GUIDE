121227fd933106fca29b9c8b4094fbf56bfcdcc7
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-30 04:24:16 +0200

    back to app - comment relative path button method

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -21,6 +21,6 @@ export class ServersComponent implements OnInit {
   }
 
   onReload() {
-    this.router.navigate(["servers"], { relativeTo: this.route });
+    //this.router.navigate(["servers"], { relativeTo: this.route });
   }
 }
