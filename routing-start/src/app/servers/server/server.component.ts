266b2f243390375652712f331bdcf38ec64a2ae4
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-30 14:43:16 +0200

    practicing and some common gotcha

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { ServersService } from '../servers.service';
+import { ServersService } from "../servers.service";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
-  selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  selector: "app-server",
+  templateUrl: "./server.component.html",
+  styleUrls: ["./server.component.css"],
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = this.route.snapshot.params["id"];
+    this.server = this.serversService.getServer(id);
+    console.log(this.server);
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params["id"]);
+    });
   }
-
 }
