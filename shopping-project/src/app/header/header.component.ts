18f87b58af2beb7be4e3fc2b686f23d007cae21e
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-17 12:58:30 +0200

    Adding Navigation to the app

--- shopping-project/src/app/header/header.component.ts
+++ shopping-project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +9,4 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-
-  // Emit event with string as data type
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 }
