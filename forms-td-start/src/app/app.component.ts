4cadefed8261ed4ef6c23ea8325cc23a9ea731f2
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-19 22:39:58 +0200

    Using ngModel with two way binding

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   //Useful to retrieve data even if we don't submit the form
   @ViewChild("f") signupForm: NgForm;
   defaultQuestion: string = "pet";
+  answer = "";
 
   suggestUserName() {
     const suggestedName = "Superuser";
