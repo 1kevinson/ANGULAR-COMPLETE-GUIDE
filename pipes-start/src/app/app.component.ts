0a9b42142a597499cd744e5ef5df66b3fd580177
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-24 15:40:18 +0200

    [Using Pipes] - Creating a filter Pipe

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -32,6 +32,9 @@ export class AppComponent {
       started: new Date(15, 1, 2017),
     },
   ];
+
+  filteredStatus: any;
+
   getStatusClasses(server: {
     instanceType: string;
     name: string;
---
+++ pipes-start/src/app/filter.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "filter",
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, propName: string, filterString: string): any {
+    const filteredArray = [];
+
+    if (value.length === 0) {
+      return value;
+    }
+
+    for (const item of value) {
+      if (item[propName] === filterString) {
+        filteredArray.push(item);
+        console.log(item);
+      }
+    }
+    return filteredArray;
+  }
+}
