82ef1f3764552aac8214b605311fe8abbf0676b5
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-25 15:12:23 +0200

    [Making Http Requests](<multiple>): Add Query Pamareters

--- http-02-post-requests/src/app/posts.service.ts
+++ http-02-post-requests/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Post } from "./post.model";
 import { catchError, map } from "rxjs/operators";
 import { Subject, throwError } from "rxjs";
@@ -27,12 +27,18 @@ export class PostsService {
   }
 
   fetchPosts() {
+    //multiple HTTPparams
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append("print", "pretty");
+    searchParams = searchParams.append("key", "value");
+
     // Pipe is use to transform the data with observable before it's handle
     return this.http
       .get<{ [key: string]: Post }>(
         "https://ng-complete-guide-59510.firebaseio.com/posts.json",
         {
           headers: new HttpHeaders({ "Custom-Header": "Hello" }),
+          params: searchParams,
         }
       )
       .pipe(
