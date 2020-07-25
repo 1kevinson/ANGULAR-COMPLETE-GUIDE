3b7c5a79c8934128ac260439cca54c7e4ba08520
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-25 23:19:34 +0200

    [Course project - Http](<firebase>): Storing recipes

--- shopping-project/src/app/header/header.component.ts
+++ shopping-project/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(private dataStorageService: DataStorageService) {}
 
   ngOnInit(): void {}
+
+  onSaveDatas() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchDatas() {}
 }
--- shopping-project/src/app/shared/data-storage.service.ts
+++ shopping-project/src/app/shared/data-storage.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { log } from 'util';
 
 // Add @Injectable it as soon we have to inject a service into this service
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://ng-course-recipe-book-3ae65.firebaseio.com/recipe.json',
+        recipes,
+      )
+      .subscribe((response) => console.log(response));
+  }
+
+  fetchRecipes() {
+
   }
 }
