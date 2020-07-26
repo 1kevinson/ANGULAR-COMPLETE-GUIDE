f8497361f090e6d2e9f72d85e0d946509d49f849
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-26 13:15:09 +0200

    [Authentication & Route Protection](<HttpErrorResponse>): Add error Handler

--- shopping-project/src/app/auth/auth.service.ts
+++ shopping-project/src/app/auth/auth.service.ts
@@ -1,16 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
-
-//Best practice : optional
-interface AuthResponseData {
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  expiresIn: string;
-  localId: string;
-}
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -29,19 +20,7 @@ export class AuthService {
       )
       .pipe(
         // Better to handle errors messages in service
-        catchError((errorRes) => {
-          let errorMsg = 'An Unknown error occured!';
-
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMsg);
-          }
-
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMsg = 'This email already exists';
-          }
-          return throwError(errorMsg);
-        })
+        catchError(this.handleError)
       );
   }
 
@@ -56,27 +35,27 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((errorRes) => {
-          let errorMsg = 'An Unknown error occured!';
+      .pipe(catchError(this.handleError));
+  }
 
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMsg);
-          }
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMsg = 'An Unknown error occured!';
 
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_NOT_FOUND':
-              errorMsg = 'This email / user is not found';
-              break;
-            case 'INVALID_PASSWORD':
-              errorMsg = 'Your password is invalid';
-              break;
-            case 'USER_DISABLED':
-              errorMsg = 'User have been disabled by Admin';
-              break;
-          }
-          return throwError(errorMsg);
-        })
-      );
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMsg);
+    }
+
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMsg = 'This email already exists';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMsg = 'This email / user is not found';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMsg = 'Your password is invalid';
+        break;
+    }
+    return throwError(errorMsg);
   }
 }
