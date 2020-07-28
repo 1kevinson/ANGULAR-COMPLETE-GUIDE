597f4ad6d2599647243b54f495e2bf946af532c2
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-28 18:13:31 +0200

    [Angular Modules & Optimizing Angular Apps](<export, NgModule>):Splitting Module Correctly

--- shopping-project/src/app/recipes/recipe.module.ts
+++ shopping-project/src/app/recipes/recipe.module.ts
@@ -5,6 +5,9 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,7 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     RecipeStartComponent,
     RecipeEditComponent,
   ],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   exports: [
     // Exports all Modules, so we can import recipe.module.ts in app.module.ts
     RecipesComponent,
